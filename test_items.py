6d19ca4d0ec7b50192e04a75fa44751e009d5d6e
Voknes <redvoknes@yandex.ru>
2022-05-17 15:49:58 +0700

    Edit test_items.py

--- test_items.py
+++ test_items.py
@@ -2,9 +2,8 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_basket(browser):
-    print(browser)
+def test_add_to_basket(browser):
     browser.get(link)
-    add_to_cart = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
-    assert len(add_to_cart) > 0, \
+    add_to_basket = browser.find_elements(By.CLASS_NAME, 'btn-add-to-basket')
+    assert len(add_to_basket) > 0, \
                'Not found!'
